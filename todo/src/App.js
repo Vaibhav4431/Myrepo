0a0a3c0aec5d422fef138c3159098f88d54c4187
Vaibhav4431 <vaibhav.k@devhostserver.com>
2023-03-14 18:14:43 +0530

    Todo List Using useReducer

--- todo/src/App.js
+++ todo/src/App.js
@@ -1,12 +1,14 @@
 // import logo from './logo.svg';
 import './App.css';
 import TodoList from "./components/TodoList"
+import TodoReducer from './components/TodoReducer';
 
 
 function App() {
   return (
     <>
-    <TodoList/>
+    {/* <TodoList/> */}
+    <TodoReducer/>
     </>
   );
 }
